aa208cd23732a17c8035a3cbeb2d4d9ec1bac614
achraf bakkou <132296411+achrafbakkou@users.noreply.github.com>
2024-06-10 23:43:03 +0100

    intro.js

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -11,7 +11,7 @@ const Intro = () => {
             <span className="hello">Hello,</span>
             <span className="introText">I'm <span className="introName">Achraf</span> <br />IT Developer</span>
             <p className="introPara">I'm a passionate IT developer who loves solving complex problems and building <br /> innovative solutions. My hardworking nature ensures I consistently <br /> deliver high-quality, efficient software.</p>
-            <button className='btn'><a href='/portfolio.pdf'><img src={btnImg} alt="My Cv" className='btnImg' />MY CV</a></button>
+            <button className='btn'><a href='/cvmy.PNG'><img src={btnImg} alt="My Cv" className='btnImg' />MY CV</a></button>
         </div>
         <img src={bg} alt="Profile" className="bg" />
     </section>
