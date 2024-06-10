c4feb59987863097e5b646ca3bd1bf3277e31623
achraf bakkou <132296411+achrafbakkou@users.noreply.github.com>
2024-06-10 23:41:09 +0100

    intro.js

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './intro.css';
 import bg from '../../AIMGGGG/pakkou.png';
 import btnImg from '../../AIMGGGG/form.png';
-import { Link } from 'react-scroll';
 
 
 const Intro = () => {
@@ -12,7 +11,7 @@ const Intro = () => {
             <span className="hello">Hello,</span>
             <span className="introText">I'm <span className="introName">Achraf</span> <br />IT Developer</span>
             <p className="introPara">I'm a passionate IT developer who loves solving complex problems and building <br /> innovative solutions. My hardworking nature ensures I consistently <br /> deliver high-quality, efficient software.</p>
-            <Link><button className="btn"><img src={btnImg} alt="My Cv" className='btnImg' />MY CV</button></Link>
+            <button className='btn'><a href='/portfolio.pdf'><img src={btnImg} alt="My Cv" className='btnImg' />MY CV</a></button>
         </div>
         <img src={bg} alt="Profile" className="bg" />
     </section>
